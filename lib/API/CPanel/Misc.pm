9250101c752f0315fda834d0f453644719ba4974
Konstantin Vlasov <bitter@cpan.org>
2010-04-05 04:37:36 +0000

    Import of BITTER/API-CPanel-0.03 from CPAN.
    gitpan-cpan-distribution: API-CPanel
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BITTER/API-CPanel-0.03.tar.gz
    gitpan-cpan-author:       BITTER
    gitpan-cpan-maturity:     released

---
+++ lib/API/CPanel/Misc.pm
@@ -0,0 +1,22 @@
+package API::CPanel::Misc;
+
+use strict;
+use warnings;
+
+use API::CPanel;
+use Data::Dumper;
+
+
+# Перегружаем CPanel
+sub reload {
+    my $params = shift;
+
+    return API::CPanel::action_abstract(
+	params         => $params,
+	func           => 'restartservice',
+	container      => 'restart',
+	allowed_fields => 'service',
+    );
+}
+
+1;
